2600d180da1a298a8990cf0fe66aa348195f0c1e
LiuZiao <burningalive.fff@live.com>
2017-09-23 02:22:19 +0800

    新添了用于首页展示的组件showcaseGroup

---
+++ src/components/showcaseGroup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container clearfix">
+    <template v-for="item of items">
+      <showcase-article :data="item" :key="item.id"></showcase-article>
+    </template>
+  </div>
+</template>
+<<script>
+import showcaseArticle from '../components/showcase-article'
+export default {
+  components: {
+    showcaseArticle
+  },
+  props: ['items']
+}
+</script>
--- src/pages/main.vue
+++ src/pages/main.vue
@@ -8,22 +8,12 @@
     <div class="container clearfix" style="padding-top: 20px;">
       <showcase-article :data="firstArticle" :size="'middle'"></showcase-article>
       <showcase-article :data="secondArticle" :size="'middle'"></showcase-article>
-      <template v-for="article in RestArticle">
-        <showcase-article :data="article" :key="article.id"></showcase-article>
-      </template>
+      <showcase-group :items="RestArticle"></showcase-group>
     </div>
     <banner cnName="新闻" enName="NEWS" id="news" imgSrc="http://alioss.g-cores.com/assets/banner/gadio-f7ed3e007552777820b9f5ffd892937cae37273efaf60593ea09e6abd851743e.jpg"></banner>
-    <div class="container">
-      <template v-for="item of news">
-        <showcase-article :data="item" :key="item.id"></showcase-article>
-      </template>
-    </div>
+    <showcase-group :items="news"></showcase-group>
     <banner cnName="文章" enName="ARTICLE" id="article" imgSrc="http://alioss.g-cores.com/assets/banner/article-e0042f16e46a464ef52cadd25f3cb7a5e5d6472467f5be6a3a357b9e355637ca.jpg"></banner>
-    <div class="container">
-      <template v-for="item of articles">
-        <showcase-article :data="item" :key="item.id"></showcase-article>
-      </template>
-    </div>
+    <showcase-group :items="articles"></showcase-group>
   </div>
 </template>
 
@@ -32,11 +22,13 @@ import showcaseArticle from '../components/showcase-article'
 import slide from '../components/slide'
 import axios from 'axios'
 import banner from '../components/banner'
+import showcaseGroup from '../components/showcaseGroup'
 export default {
   components: {
     showcaseArticle,
     slide,
-    banner
+    banner,
+    showcaseGroup
   },
   data () {
     return {
@@ -60,23 +52,23 @@ export default {
       self.firstArticle = res.data.shift()
       self.secondArticle = res.data.shift()
       self.RestArticle = res.data
-      console.dir(self.firstArticle)
-      console.dir(self.secondArticle)
     }, function (err) {
       console.log(err)
     })
-    // this.$http.post('http://localhost:6666/Psy/selectAllNews.htm')
-    // .then((res) => {
-    //   this.news = res
-    // }, (err) => {
-    //   this.$message.error(err)
-    // })
-    // this.$http.post('http://localhost:6666/Psy/selectAllArticle.htm')
-    // .then((res) => {
-    //   this.articles = res
-    // }, (err) => {
-    //   this.$messsage.error(err)
-    // })
+    this.$http.post('http://localhost:6666/Psy/selectAllNews.htm')
+    .then((res) => {
+      this.news = res.data
+    }, (err) => {
+      this.$message.error(err)
+    })
+    this.$http.post('http://localhost:6666/Psy/selectAllArticle.htm')
+    .then((res) => {
+      this.articles = res.data
+      console.log('selectAllArticles')
+      console.dir(res)
+    }, (err) => {
+      this.$messsage.error(err)
+    })
   }
 }
 </script>
