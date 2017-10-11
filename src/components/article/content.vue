9bfac2fdb26bf50fb104778e82cdd24b12c89f2e
LiuZiao <burningalive.fff@live.com>
2017-10-11 22:36:05 +0800

    视频样式修复

--- src/components/article/content.vue
+++ src/components/article/content.vue
@@ -17,19 +17,7 @@
 i, cite, em, var, address, dfn {
     font-style: italic;
 }
-.article-content p {
-  line-height: 1.8;
-  font-size: 16px;
-}
-.story {
-  max-width: 700px;
-  margin: 70px auto;
-  padding: 0 15px;
-  font-size: 16px;
-  line-height: 1.8;
-  word-break: break-all;
-  word-wrap: break-word;
-}
+
 .story_elem {
     margin: 20px 0;
 }
@@ -72,6 +60,13 @@ h3 {
   font-size: 24px;
   font-family: "Open Sans", "Helvetica Neue", Arial, "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
 }
+.article-content p {
+  line-height: 1.8;
+  font-size: 16px;
+  strong{
+    font-size: inherit;
+  }
+}
 .story_caption {
     text-align: center;
     font-size: 12px;
@@ -87,6 +82,200 @@ h3 {
 .story h4, .story .h4, .story h5, .story .h5, .story h6, .story .h6 {
     margin-top: 34.5px;
     margin-bottom: 23px;
+}
+.story {
+  max-width: 700px;
+  margin: 70px auto;
+  padding: 0 15px;
+  font-size: 16px;
+  line-height: 1.8;
+  word-break: break-all;
+  word-wrap: break-word;
+}
+blockquote {
+    padding: 11.5px 23px;
+    margin: 0 0 23px;
+    font-size: 17.5px;
+    border-left: 5px solid #f0f0f0;
+}
+.td-w-sm .td-header, .td-w-sm .td-header .td-header-container, .td-w-sm .td__footer {
+    min-width: 1080px !important;
+}
+.td-header {
+    font-size: 14px;
+    width: 100%;
+    min-width: 908px;
+    height: 60px;
+}
+.td-header-container {
+    min-width: 908px;
+    height: 40px;
+    padding: 10px 0;
+    background: #f60;
+}
+.td-logo-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 415px;
+    height: 60px;
+    overflow: hidden;
+}
+.td-right-bg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 410px;
+    height: 60px;
+    overflow: hidden;
+}
+.td-header-container .td-box {
+    z-index: 1290;
+    position: relative;
+    padding: 0 40px;
+    height: 40px;
+    zoom: 1;
+    margin: 0 auto;
+    min-width: 828px;
+}
+.td-logo {
+    float: left;
+}
+.td-header a {
+    text-decoration: none;
+}
+.td-w-sm .td-search {
+    left: 40%;
+    width: 380px;
+}
+.td-search {
+    position: absolute;
+    height: 36px;
+    top: 50%;
+    left: 50%;
+    margin-left: -227px;
+    margin-top: -18px;
+    display: block;
+    width: 452px;
+}
+.td-head-right {
+    float: right;
+}
+.td-mytd {
+    float: right;
+    width: 60px;
+    height: 16px;
+    line-height: 16px;
+    padding: 6px 15px;
+    text-align: center;
+    border: 1px solid rgba(255,255,255,0.45);
+    -webkit-border-radius: 100px;
+    -moz-border-radius: 100px;
+    border-radius: 100px;
+    margin-top: 5px;
+}
+.td-mytd .td-mytd-msg:link {
+    color: rgba(255,255,255,0.9);
+}
+.td-mytd .td-mytd-msg:hover, .td-mytd .td-mytd-msg:visited {
+    color: #fff;
+}
+.td-login, .td-notice {
+    position: relative;
+    float: right;
+}
+.login-before {
+    cursor: pointer;
+}
+.login-before {
+    float: left;
+    min-width: 66px;
+    margin-right: 20px;
+    padding-top: 8px;
+    position: relative;
+}
+.login-before .msg-icon {
+    display: inline-block;
+    margin-top: -4px;
+    width: 34px;
+    height: 34px;
+    line-height: 34px;
+    font-size: 34px;
+    text-align: center;
+    color: #f1f1f1;
+    -webkit-border-radius: 100%;
+    -moz-border-radius: 100%;
+    border-radius: 100%;
+    vertical-align: middle;
+}
+.login-before .msg-text {
+    color: #fff;
+    font-size: 14px;
+    line-height: 14px;
+    padding-left: 5px;
+}
+.td-login-pop {
+    top: 53px;
+    right: 33px;
+    display: none;
+    padding: 12px 80px 12px 18px;
+    line-height: 22px;
+    height: 22px;
+}
+.td-h__upload {
+    padding-right: 20px;
+}
+.td-login, .td-notice {
+    position: relative;
+    float: right;
+}
+.td-w-sm .td-play__container {
+    padding-top: 20px;
+    width: 1080px;
+}
+.td-play__container {
+    padding-bottom: 40px;
+}
+.td-container, .td-play__container {
+    margin: 0 auto;
+    padding: 20px 0;
+}
+.td-row, .td-container, .td-play__container {
+    margin-right: -10px;
+    margin-left: -10px;
+    font-size: 0;
+    vertical-align: top;
+}
+.td-playarea--l {
+    float: left;
+}
+.td-playarea {
+    position: relative;
+    margin-bottom: 20px;
+}
+.td-w-sm .td-interactbox, .td-w-sm .td-playbox {
+    width: 665px;
+}
+.td-w-sm .td-playbox {
+    height: 374.0625px;
+}
+.td-playbox {
+    background: #000000;
+}
+.story_elem-embed {
+    padding: 0 15px;
+    margin: 30px 0;
+}
+.story_elem-embed>[allowfullscreen], .story_elem-embed>[id="ACFlashPlayer-re"] {
+@media (min-width: 768px){
+    width: 640px !important;
+    height: 424px !important;}
+}
+
+
+.story_elem-embed>embed, .story_elem-embed>iframe, .story_elem-embed>object {
+    display: block;
+    margin: 0 auto;
 }
 </style>
 
